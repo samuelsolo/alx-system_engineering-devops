b5674283e7104d23284ce77a3d1752b82fa0497e
samuelsolo <samuelsolomon.des@gmail.com>
2024-07-13 13:03:38 -0700

    0x1B-web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-ncreases the open file limit for nginx
+# increases the open file limit for nginx
 exec { 'fix file limit':
   onlyif  => 'test -e /etc/default/nginx',
   command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
